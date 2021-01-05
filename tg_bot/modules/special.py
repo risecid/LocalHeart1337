dfbe201c646d1351bd213f9110bfe6fb97999bb4
Nizar Rian <garudaindonesia710@gmail.com>
2021-01-05 21:09:32 +0700

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -64,10 +64,10 @@ def banall(bot: Bot, update: Update, args: List[int]):
 @run_async
 def snipe(bot: Bot, update: Update, args: List[str]):
     try:
-        chat_id = str(args[1])
+        chat_id = str(args[0])
     except TypeError as excp:
         update.effective_message.reply_text("Please give me a chat to echo to!")
-    to_send = " ".join(args)
+    to_send = "".join(args)
     if len(to_send) >= 2:
         try:
             bot.sendMessage(int(chat_id), str(to_send))
