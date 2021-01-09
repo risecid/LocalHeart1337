1a29ff49b311fe6f05fb728b37c57e917dca5635
Nizar Rian <garudaindonesia710@gmail.com>
2021-01-09 15:20:27 +0700

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -322,7 +322,7 @@ def echo(bot: Bot, update: Update):
     message.delete()
 
 @run_async
-# def kimak(bot: Bot, update: Update):
+def kimak(bot: Bot, update: Update):
     url = 'https://secureapp.simsimi.com/v1/simsimi/talkset?uid=297390035&av=6.9.3.7&lc=id&cc=ID&tz=Asia%2FJakarta&os=a&ak=Nsh1x94iNA2oftvixJMmTj1awEk%3D&message_sentence={}&normalProb=8&isFilter=1&talkCnt=8&talkCntTotal=8&reqFilter=1&session=MMWBpntzK2hS64aX7SuhQHTcsHCrVftmwJBk7cGd3ViCyVTFx4ywxuEvvTHnQWrt9ENooUhdQXaD6XrDuTyGbSv2&triggerKeywords=%5B%5D'.format(update.effective_message.text)
     r = requests.get(url)
     data = json.loads(r.text)
